fcdaf379d42bdf90b6f4349589e0410c47d36f79
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-10-27 19:28:44 +0600

    dart app

---
+++ dart_application_271023/bin/dart01.dart
@@ -0,0 +1,13 @@
+void main() {
+  for (var index = 0; index < 5; index++) {
+    print(index);
+  }
+  ;
+
+  var list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  print(list1);
+
+  for (var item in list1) {
+    print(item);
+  }
+}
