ef815c1ad86b60b6ab8c10d97a8d3aa479c1a19c
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-10-27 11:07:23 +0600

    dart app

---
+++ dart_application_271023/bin/dart_application_271023.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_271023/dart_application_271023.dart' as dart_application_271023;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_271023.calculate()}!');
+}
---
+++ dart_application_271023/test/dart_application_271023_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_application_271023/dart_application_271023.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
