8257867f50781a57bdb42d62d8da93d09fef7116
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-10-28 19:59:59 +0600

    comment

--- dart_application_271023/bin/dart_application_271023.dart
+++ dart_application_271023/bin/dart_application_271023.dart
@@ -5,74 +5,92 @@ import 'dart:io';
 import 'ddd.dart';
 //// users list
 
-Map user = {
-  'name': 'User1',
-  'lastName': 'User1User',
-  'age': 33,
-  'login': 'user@gmail.com',
-  'password': '1234',
-};
+// Map user = {
+//   'name': 'User1',
+//   'lastName': 'User1User',
+//   'age': 33,
+//   'login': 'user@gmail.com',
+//   'password': '1234',
+// };
 
 ////
 void main(List<String> arguments) {
-  // print('Hello world: ${dart_application_271023.calculate()}!');
-
   authentification();
 }
 
 void authentification() {
-// final list = [user];
-// var
+  final userler = [];
+  var quit = '0';
 
-  // welcome
-  print('Welcome!!!');
+  do {
+    // welcome
+    print('Welcome!!!');
 
-  // log/registr
-  print('Kattaluu uchun "Y". Kiruu uchun "N" jazynyz');
-  var value = stdin.readLineSync();
-  if (value == 'Y') {
-    print('Kattaluu bolumundo');
+    // log/registr
+    print('Kattaluu uchun "Y". Kiruu uchun "N" jazynyz');
+    var value = stdin.readLineSync();
+    if (value == 'Y') {
+      print('Kattaluu bolumundo');
 
-    stdout.write('Enter your name: ');
-    var name = stdin.readLineSync();
+      stdout.write('Enter your name: ');
+      var name = stdin.readLineSync();
 
-    stdout.write('Enter your last name: ');
-    var lastName = stdin.readLineSync();
+      stdout.write('Enter your last name: ');
+      var lastName = stdin.readLineSync();
 
-    stdout.write('Enter your age: ');
-    var age = stdin.readLineSync();
+      stdout.write('Enter your age: ');
+      var age = stdin.readLineSync();
 
-    stdout.write('M/F: ');
-    var maleF = stdin.readLineSync();
+      stdout.write('M/F: ');
+      var maleF = stdin.readLineSync();
 
-    stdout.write('Enter your login: ');
-    var login = stdin.readLineSync();
+      stdout.write('Enter your login: ');
+      var login = stdin.readLineSync();
 
-    stdout.write('Enter your login: ');
-    var password = stdin.readLineSync();
+      stdout.write('Enter your password: ');
+      var password = stdin.readLineSync();
 
-    print('name is $name');
-    print('last name is $lastName');
-    print('your age is $age');
-    print('your age is $maleF');
-    print('your age is $login');
-    print('your age is $password');
-    print('Done');
-  } else if (value == 'N') {
-    print('Kiruu bolumundo');
+      print('name is $name');
+      print('last name is $lastName');
+      print('your age is $age');
+      print('your age is $maleF');
+      print('your age is $login');
+      print('your age is $password');
 
-    stdout.write('Enter your login: ');
-    var login = stdin.readLineSync();
+      var newUser = {
+        'name': user,
+        'lastName': lastName,
+        'age': age,
+        'login': login,
+        'password': password,
+      };
 
-    stdout.write('Password your login: ');
-    var password = stdin.readLineSync();
+      userler.add(newUser);
 
-    if (login == user['login'] && password == user['password']) {
-      print('User name is correct');
+      stdout.write('Exit 1, ulantuu Enter');
+      quit = stdin.readLineSync() ?? '';
+
+      print('Done');
+    } else if (value == 'N') {
+      print('Kiruu bolumundo');
+
+      stdout.write('Enter your login: ');
+      var login = stdin.readLineSync();
+
+      stdout.write('Password your login: ');
+      var password = stdin.readLineSync();
+
+      for (var user in userler) {
+        if (login == user['login'] && password == user['password']) {
+          print('User name is correct: $user');
+        } else {
+          print('Wrong login or password');
+        }
+      }
+      stdout.write('Exit 1, ulantuu Enter');
+      quit = stdin.readLineSync() ?? '';
     } else {
-      print('Wrong login or password');
+      print(value);
     }
-  } else {
-    print(value);
-  }
+  } while (quit != '1');
 }
