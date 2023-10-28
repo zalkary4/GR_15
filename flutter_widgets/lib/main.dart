f18a21a6f8e3766339a091fc0d3202a6ddef8199
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-10-28 21:54:28 +0600

    comment

--- flutter_widgets/lib/main.dart
+++ flutter_widgets/lib/main.dart
@@ -30,7 +30,19 @@ class MyHomePage extends StatelessWidget {
       backgroundColor: const Color(0xffecb90b),
       appBar: AppBar(
         elevation: 2.2,
+        shadowColor: Colors.black,
+        title: const Text(
+          'Task 3',
+          style: TextStyle(color: Colors.black),
+        ),
         backgroundColor: Colors.brown,
+      ),
+      body: Column(
+        children: [
+          Text('Rich'),
+          Image.network(
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThX5BgHRR1N_HZgIq_TALqgY7t6fuyR9Qo7Q&usqp=CAU')
+        ],
       ),
     );
   }
