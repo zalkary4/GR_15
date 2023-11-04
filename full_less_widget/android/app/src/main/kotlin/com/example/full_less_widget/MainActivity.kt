bcab4969194f3208852da4f248c2a6b0dd2bb927
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-11-04 18:46:41 +0600

    comment

---
+++ full_less_widget/android/app/src/main/kotlin/com/example/full_less_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.full_less_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ full_less_widget/lib/home2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePageFull extends StatefulWidget {
+  const HomePageFull({super.key});
+
+  @override
+  State<HomePageFull> createState() => _HomePageFullState();
+}
+
+class _HomePageFullState extends State<HomePageFull> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
