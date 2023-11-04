6d5a52f6f0f247d73599f65912e161da11bc6ddf
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-11-04 18:49:15 +0600

    comment

--- full_less_widget/lib/home1.dart
+++ full_less_widget/lib/home1.dart
@@ -10,6 +10,16 @@ class HomePageLess extends StatelessWidget {
         backgroundColor: Colors.blue,
         title: Text('Less Widget'),
       ),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('0'),
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
