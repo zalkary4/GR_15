b6ea7e54b99631d905b070bd82d87b5f57c91fd2
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-11-05 15:34:41 +0600

    comment

--- full_less_widget/lib/main.dart
+++ full_less_widget/lib/main.dart
@@ -4,9 +4,15 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isDark = true;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,87 +21,16 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.deepPurple,
-          brightness: Brightness.dark,
+          brightness: isDark ? Brightness.dark : Brightness.light,
         ),
         useMaterial3: true,
       ),
-      home: ThemeDataDart(),
-    );
-  }
-}
-
-class ThemeDataDart extends StatelessWidget {
-  const ThemeDataDart({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // backgroundColor: Theme.of(context).colorScheme.background,
-        title: const Text('State Less Widget'),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.light_mode),
-          ),
-        ],
-      ),
-      body: const Center(
-        child: CounterRebuild(),
-      ),
-    );
-  }
-}
-
-class CounterRebuild extends StatefulWidget {
-  const CounterRebuild({
-    super.key,
-  });
-
-  @override
-  State<CounterRebuild> createState() => _CounterRebuildState();
-}
-
-class _CounterRebuildState extends State<CounterRebuild> {
-  int count = 0;
-  @override
-  Widget build(BuildContext context) {
-    print('great State:  $count++');
-    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Card(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 150,
-            vertical: 20,
-          ),
-          child: Text(
-            'San: $count ',
-            style: TextStyle(fontSize: 24),
-          ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text('State Less Widget'),
         ),
       ),
-      const SizedBox(height: 30),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              count--;
-              print(count);
-              setState(() {});
-            },
-            child: Icon(Icons.remove),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              count++;
-              print(count);
-              setState(() {});
-            },
-            child: Icon(Icons.add),
-          )
-        ],
-      )
-    ]);
+    );
   }
 }
