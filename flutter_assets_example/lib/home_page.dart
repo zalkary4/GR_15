934ef37568cfa5dd06e3e60f5cc57527339f9eba
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-11-08 14:51:41 +0600

    assets

---
+++ flutter_assets_example/lib/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Assets'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Flutter',
+            style: TextStyle(
+              fontSize: 48,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Sofia',
+            ),
+          ),
+          Image.asset('assets/images/venice.jpg')
+        ],
+      ),
+    );
+  }
+}
