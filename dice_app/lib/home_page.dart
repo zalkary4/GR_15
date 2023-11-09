ea8da7893e3202b108e7b2dadd47367b1099e785
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-11-09 19:38:58 +0600

    done

--- dice_app/lib/home_page.dart
+++ dice_app/lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -16,18 +18,43 @@ class HomePage extends StatelessWidget {
         title: const Text('Dice App'),
         backgroundColor: const Color(0xffda0037),
       ),
-      body: Center(
+      body: HomePageBody(),
+    );
+  }
+}
+
+class HomePageBody extends StatefulWidget {
+  HomePageBody({super.key});
+  @override
+  State<HomePageBody> createState() => _HomePageBodyState();
+}
+
+class _HomePageBodyState extends State<HomePageBody> {
+  int firstDiceIndex = 5;
+  int secondDiceIndex = 2;
+  Random random = Random();
+  void changeDice() {
+    firstDiceIndex = random.nextInt(6) + 1;
+    secondDiceIndex = random.nextInt(6) + 1;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: InkWell(
+        onTap: changeDice,
         child: Row(children: [
           const SizedBox(width: 20),
           Expanded(
             child: Image.asset(
-              'assets/icons/dice1.png',
+              'assets/icons/dice$firstDiceIndex.png',
             ),
           ),
           const SizedBox(width: 20),
           Expanded(
             child: Image.asset(
-              'assets/icons/dice2.png',
+              'assets/icons/dice$secondDiceIndex.png',
             ),
           ),
           const SizedBox(width: 20),
