eb5ffd771c4c0f51c88b78b27dcaa3f8b1797a87
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-11-11 19:56:01 +0600

    piano

---
+++ hylophon_app/android/app/src/main/kotlin/com/example/hylophon_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hylophon_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ hylophon_app/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:hylophon_app/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
