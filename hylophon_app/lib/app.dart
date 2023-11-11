cd0d467c1e2d82a42b6c249899839e0c44c1932f
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-11-11 22:20:22 +0600

    piano

--- hylophon_app/lib/app.dart
+++ hylophon_app/lib/app.dart
@@ -8,11 +8,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
+      theme: ThemeData.dark(useMaterial3: true),
+      home: HomePage(),
     );
   }
 }
--- hylophon_app/lib/home_page.dart
+++ hylophon_app/lib/home_page.dart
@@ -1,15 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
+final player = AudioPlayer();
+
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Piano',
+            style: TextStyle(fontWeight: FontWeight.bold)),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: InkWell(
+              onTap: () {
+                player.play(
+                  AssetSource('audios/note1.wav'),
+                );
+              },
+              child: Container(
+                color: const Color(0xffef4434),
+              ),
+            ),
+          ),
+          Expanded(
+            child: InkWell(
+              onTap: () {
+                player.play(
+                  AssetSource('audios/note2.wav'),
+                );
+              },
+              child: Container(
+                color: const Color(0xffef99700),
+              ),
+            ),
+          ),
+          Expanded(
+            child: InkWell(
+              onTap: () {
+                player.play(
+                  AssetSource('audios/note3.wav'),
+                );
+              },
+              child: Container(
+                color: const Color(0xffffe93b),
+              ),
+            ),
+          ),
+          Expanded(
+            child: InkWell(
+              onTap: () {
+                player.play(
+                  AssetSource('audios/note4.wav'),
+                );
+              },
+              child: Container(
+                color: const Color(0xff4CB050),
+              ),
+            ),
+          ),
+          Expanded(
+            child: InkWell(
+              onTap: () {
+                player.play(
+                  AssetSource('audios/note5.wav'),
+                );
+              },
+              child: Container(
+                color: const Color(0xff2E968C),
+              ),
+            ),
+          ),
+          Expanded(
+            child: InkWell(
+              onTap: () {
+                player.play(
+                  AssetSource('audios/note6.wav'),
+                );
+              },
+              child: Container(
+                color: const Color(0xff2996F5),
+              ),
+            ),
+          ),
+          Expanded(
+            child: InkWell(
+              onTap: () {
+                player.play(
+                  AssetSource('audios/note7.wav'),
+                );
+              },
+              child: Container(
+                color: const Color(0xff9B28B1),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {},
+      ),
+      bottomNavigationBar: const SizedBox(height: 70),
+    );
   }
 }
