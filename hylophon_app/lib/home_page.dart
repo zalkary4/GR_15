489d9784e127a73a41dff31cea3045a0b6ae7065
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-11-11 22:46:30 +0600

    piano

--- hylophon_app/lib/home_page.dart
+++ hylophon_app/lib/home_page.dart
@@ -20,89 +20,40 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Column(
         children: [
-          Expanded(
-            child: InkWell(
-              onTap: () {
-                player.play(
-                  AssetSource('audios/note1.wav'),
-                );
-              },
-              child: Container(
-                color: const Color(0xffef4434),
-              ),
-            ),
+          AudioItem(
+            note: 'note1',
+            color: const Color(0xffEF443A),
+            player: player,
           ),
-          Expanded(
-            child: InkWell(
-              onTap: () {
-                player.play(
-                  AssetSource('audios/note2.wav'),
-                );
-              },
-              child: Container(
-                color: const Color(0xffef99700),
-              ),
-            ),
+          AudioItem(
+            note: 'note2',
+            color: const Color(0xffEf9970),
+            player: player,
           ),
-          Expanded(
-            child: InkWell(
-              onTap: () {
-                player.play(
-                  AssetSource('audios/note3.wav'),
-                );
-              },
-              child: Container(
-                color: const Color(0xffffe93b),
-              ),
-            ),
+          AudioItem(
+            note: 'note3',
+            color: const Color(0xffffe93b),
+            player: player,
           ),
-          Expanded(
-            child: InkWell(
-              onTap: () {
-                player.play(
-                  AssetSource('audios/note4.wav'),
-                );
-              },
-              child: Container(
-                color: const Color(0xff4CB050),
-              ),
-            ),
+          AudioItem(
+            note: 'note4',
+            color: const Color(0xff4CB050),
+            player: player,
           ),
-          Expanded(
-            child: InkWell(
-              onTap: () {
-                player.play(
-                  AssetSource('audios/note5.wav'),
-                );
-              },
-              child: Container(
-                color: const Color(0xff2E968C),
-              ),
-            ),
+          AudioItem(
+            note: 'note5',
+            color: const Color(0xff2E968C),
+            player: player,
           ),
-          Expanded(
-            child: InkWell(
-              onTap: () {
-                player.play(
-                  AssetSource('audios/note6.wav'),
-                );
-              },
-              child: Container(
-                color: const Color(0xff2996F5),
-              ),
-            ),
+          AudioItem(
+            note: 'note6',
+            color: const Color(0xff2996F5),
+            player: player,
           ),
-          Expanded(
-            child: InkWell(
-              onTap: () {
-                player.play(
-                  AssetSource('audios/note7.wav'),
-                );
-              },
-              child: Container(
-                color: const Color(0xff9B28B1),
-              ),
-            ),
+          AudioItem(
+            note: 'note7',
+            color: const Color(0xff9B28B1),
+            player: player,
           ),
         ],
       ),
@@ -110,6 +61,35 @@ class _HomePageState extends State<HomePage> {
         onPressed: () async {},
       ),
       bottomNavigationBar: const SizedBox(height: 70),
+    );
+  }
+}
+
+class AudioItem extends StatelessWidget {
+  const AudioItem({
+    super.key,
+    required this.player,
+    required this.color,
+    required this.note,
+  });
+
+  final AudioPlayer player;
+  final Color color;
+  final String note;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: InkWell(
+        onTap: () {
+          player.play(
+            AssetSource('audios/$note.wav'),
+          );
+        },
+        child: Container(
+          color: color,
+        ),
+      ),
     );
   }
 }
