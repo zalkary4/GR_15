d30edb2f10a91ebae061ff40ebe93a9c460c3281
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-11-11 23:26:55 +0600

    piano app

--- piano_app/lib/home_page.dart
+++ piano_app/lib/home_page.dart
@@ -27,13 +27,45 @@ class _HomePageState extends State<HomePage> {
           const Spacer(),
           Row(
             children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(fixedSize: const Size(60, 250)),
-                onPressed: () {},
-                child: const Text('f1'),
-              )
+              PianoWhiteButton(text: 'f1'),
+              PianoWhiteButton(text: 'f2'),
+              PianoWhiteButton(text: 'f3'),
+              PianoWhiteButton(text: 'f4'),
+              PianoWhiteButton(text: 'f5'),
+              PianoWhiteButton(text: 'f6'),
+              PianoWhiteButton(text: 'f7'),
             ],
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class PianoWhiteButton extends StatelessWidget {
+  const PianoWhiteButton({
+    super.key,
+    required this.text,
+  });
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        fixedSize: const Size(60, 260),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+        ),
+      ),
+      onPressed: () {},
+      child: Column(
+        children: [
+          const Spacer(),
+          Text(text),
+          const SizedBox(height: 10),
         ],
       ),
     );
