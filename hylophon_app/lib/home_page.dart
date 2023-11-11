d9b49ebf2aebcf363beb256dacf01466c50e16b3
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-11-11 22:47:29 +0600

    piano

--- hylophon_app/lib/home_page.dart
+++ hylophon_app/lib/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({super.key});
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
