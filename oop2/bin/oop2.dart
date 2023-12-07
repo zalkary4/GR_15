3a3e6065f98eacb7e40a8b8138bddc2edb7b7c46
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-12-07 14:44:28 +0600

    Day 31

--- oop2/bin/oop2.dart
+++ oop2/bin/oop2.dart
@@ -1,5 +1,27 @@
-import 'package:oop2/oop2.dart' as oop2;
+// void main(List<String> arguments) {
+//   print('Hello world: ${oop2.calculate()}!');
+// }
+void main() {
+  Dog tuzik = Dog();
+  tuzik.name = 'Tuzik';
+  tuzik.unChygar();
 
-void main(List<String> arguments) {
-  print('Hello world: ${oop2.calculate()}!');
+  Cat kisa = Cat();
+  kisa.name = 'Kisa';
+  kisa.unChygar();
+}
+
+class Dog {
+  String? name;
+
+  void unChygar() {
+    print('Gav gav');
+  }
+}
+
+class Cat {
+  String? name;
+  void unChygar() {
+    print('Meo meo');
+  }
 }
