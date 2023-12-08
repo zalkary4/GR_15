ffaa2df271f4adc2684d2dfb3d8d52efb7fce54d
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-12-08 16:22:14 +0600

    Day 32

---
+++ test_app/lib/suroo.dart
@@ -0,0 +1,53 @@
+class Suroo {
+  Suroo({
+    required this.text,
+    required this.joop,
+  });
+  final String text;
+  final bool joop;
+}
+
+final s1 = Suroo(
+  text: 'Кыргызстанда 7 область барбы?',
+  joop: true,
+);
+
+final s2 = Suroo(
+  text: 'Ысык-Колдун терендиги 668 метрби?',
+  joop: true,
+);
+
+final s3 = Suroo(
+  text: 'Кыргызстандын борбору Ош шаарыбы?',
+  joop: false,
+);
+
+final s4 = Suroo(
+  text: 'День программисте 14 сентября?',
+  joop: false,
+);
+
+final s5 = Suroo(
+  text: 'Кыргызстанда эн бийик чоку Ленинби',
+  joop: false,
+);
+final s6 = Suroo(
+  text: 'Эн чон планетабы Юпитерби',
+  joop: true,
+);
+final s7 = Suroo(
+  text: 'Кыргызстан Евразия материгинде жайгашканбы?',
+  joop: true,
+);
+final s8 = Suroo(
+  text: 'Кыргызстанда Flutter Developer барбы?',
+  joop: true,
+);
+final s9 = Suroo(
+  text: 'Сегодя выходной?',
+  joop: false,
+);
+final s10 = Suroo(
+  text: 'Много ли программистов в Кыргызстане',
+  joop: false,
+);
