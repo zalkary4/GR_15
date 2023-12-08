3ad429565905280b872cea324a940663f52f234a
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-12-08 13:48:18 +0600

    Day 31

--- oop2/bin/stud_aktan.dart
+++ oop2/bin/stud_aktan.dart
@@ -29,6 +29,31 @@ class StudentNamed {
     this.name,
     this.age,
     this.special,
+    this.kurs,
+    this.university,
+    this.avaregeScore,
+  });
+  String? id;
+  String? name;
+  int? age;
+  String? special;
+  int? kurs;
+  String? university;
+  double? avaregeScore;
+
+  void taanysh() {
+    print(
+      'My name is $name, Jashym $age, $university $kurs okuim, kesibim $special, ortocho baam $avaregeScore',
+    );
+  }
+}
+
+class StudentNameDefault {
+  StudentNameDefault({
+    this.id,
+    this.name,
+    this.age = 19,
+    this.special,
     this.kurs = 1,
     this.university = 'Manas',
     this.avaregeScore,
