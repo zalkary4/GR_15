5659d5da05c7cfcce7ceffe8e720ef04064a4758
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-12-08 15:19:26 +0600

    Day 32

--- test_app/lib/home.dart
+++ test_app/lib/home.dart
@@ -5,6 +5,10 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Task 7'),
+      ),
+    );
   }
 }
