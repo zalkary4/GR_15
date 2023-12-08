d8e54f8d08cba44a87b593a80cdd249a525a27cd
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-12-08 15:24:39 +0600

    Day 32

--- test_app/lib/home.dart
+++ test_app/lib/home.dart
@@ -7,7 +7,22 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Task 7'),
+        backgroundColor: Color(0xff343434),
+        title: const Text(
+          'Task 7',
+          style: TextStyle(fontWeight: FontWeight.w700),
+        ),
+      ),
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Кыргызстанда канча область бар?',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Colors.white, fontSize: 32, fontWeight: FontWeight.w500),
+          )
+        ],
       ),
     );
   }
