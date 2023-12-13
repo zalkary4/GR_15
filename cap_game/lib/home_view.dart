0e8f4f3069c5034c82b75b24bad96bed582cac5b
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-12-13 17:21:34 +0600

    Day 37

--- cap_game/lib/home_view.dart
+++ cap_game/lib/home_view.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Capital'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.more_vert,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset('assets/icons/settings.svg'),
+          ),
+        ],
+      ),
+    );
   }
 }
