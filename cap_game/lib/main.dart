b9753df5dada8be5855f9391317b5348f036b707
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-12-13 17:13:54 +0600

    Day 37

--- cap_game/lib/main.dart
+++ cap_game/lib/main.dart
@@ -1,21 +1,6 @@
+import 'package:cap_game/app/meta_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const SizedBox(),
-    );
-  }
 }
