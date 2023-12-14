9af0b69e4f4beee3bcbb364dd993b44514b79b3c
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-12-14 17:15:52 +0600

    Day 38

--- cap_game/lib/models/question.dart
+++ cap_game/lib/models/question.dart
@@ -1,8 +1,15 @@
 class Test {
+  Test(
+      {required this.questionText,
+      required this.variant1,
+      required this.variant2,
+      required this.variant3,
+      required this.variant4});
   final String questionText;
-  final String joop;
-
-  // firstVariant;
+  final Variant variant1;
+  final Variant variant2;
+  final Variant variant3;
+  final Variant variant4;
 }
 
 class Variant {
