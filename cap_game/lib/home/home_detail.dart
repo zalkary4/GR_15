9a67efb943d5f8f94917eaa75cdd4c0aaf49c115
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-12-14 15:44:40 +0600

    Day 38

--- cap_game/lib/home/home_detail.dart
+++ cap_game/lib/home/home_detail.dart
@@ -16,44 +16,60 @@ class _HomeDetailViewState extends State<HomeDetailView> {
         title: const AppBarTitle(title: 'Asia'),
       ),
       body: Column(
-        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           const Text(
             'Bishkek',
             style: TextStyle(fontSize: 38),
           ),
           Padding(
-            padding: EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
             child: Image.network('src'),
           ),
           const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          const Row(
             children: [
-              ElevatedButton(
-                onPressed: () {},
-                child: Text('KG'),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text('KZ'),
-              ),
+              OptionButton(),
+              OptionButton(),
             ],
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          const Row(
             children: [
-              ElevatedButton(
-                onPressed: () {},
-                child: Text('UZ'),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text('TR'),
-              ),
+              OptionButton(),
+              OptionButton(),
             ],
           ),
+          SizedBox(
+            height: 40,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (BuildContext context, int index) {
+                return const Icon(Icons.star);
+              },
+            ),
+          )
         ],
+      ),
+    );
+  }
+}
+
+class OptionButton extends StatelessWidget {
+  const OptionButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+              fixedSize: const Size(double.infinity, 70),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20))),
+          child: const Text('KG'),
+        ),
       ),
     );
   }
