c4e1bf501fec07822b5953d9be2a95e9cf36fc69
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-12-14 17:13:43 +0600

    Day 38

---
+++ cap_game/lib/models/question.dart
@@ -0,0 +1,15 @@
+class Test {
+  final String questionText;
+  final String joop;
+
+  // firstVariant;
+}
+
+class Variant {
+  const Variant({
+    required this.text,
+    this.isTrue = false,
+  });
+  final String text;
+  final bool isTrue;
+}
