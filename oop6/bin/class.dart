7411e4fa18acfcaa6756f953fe84de1964cc18fb
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-12-19 16:08:49 +0600

    Day 41

--- oop6/bin/class.dart
+++ oop6/bin/class.dart
@@ -1,7 +1,24 @@
+import 'package:test/test.dart';
+
 class Animal {
   const Animal(this.name);
   final String name;
   void jashayt() {}
+  void golos() {
+    print('');
+  }
 }
 
-class Cat extends Animal {}
+class Cat extends Animal {
+  Cat(super.name);
+  @override
+  void golos() {
+    print('myauu');
+    super.golos();
+  }
+}
+
+void main(List<String> args) {
+  final cat1 = Cat('Tom');
+  print(cat1.name);
+}
