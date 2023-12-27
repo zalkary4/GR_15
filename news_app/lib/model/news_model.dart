3747c9a37e8cc8437cb00bb7f79aa9ce1f150900
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-12-27 16:17:45 +0600

    Day 41

---
+++ news_app/lib/model/news_model.dart
@@ -0,0 +1,12 @@
+class NewModel {
+  NewModel({
+    required this.title,
+    required this.description,
+    required this.dateTime,
+    required this.image,
+  });
+  final String title;
+  final String description;
+  final String image;
+  final String dateTime;
+}
