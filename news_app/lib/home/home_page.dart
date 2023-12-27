a06e1eb6810032f21e4db7daa31f6a90c44fdf34
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-12-27 17:14:52 +0600

    Day 41

--- news_app/lib/home/home_page.dart
+++ news_app/lib/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/constants/app_colors.dart';
+import 'package:news_app/model/news_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -22,6 +23,41 @@ class _HomePageState extends State<HomePage> {
             icon: const Icon(Icons.more_vert),
           ),
         ],
+      ),
+      body: ListView.builder(
+        itemCount: newsFakeList.length,
+        itemBuilder: (context, index) {
+          final NewModel = newsFakeList[index];
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: SizedBox(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Image.network(
+                      NewModel.image,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(width: 15),
+                  Expanded(
+                    flex: 3,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(NewModel.dateTime),
+                        Text(NewModel.description),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
