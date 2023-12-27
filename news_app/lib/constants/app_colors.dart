4486b4777ccffa940016a16e3d38bd586fc9b746
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-12-27 16:12:52 +0600

    Day 41

--- news_app/lib/constants/app_colors.dart
+++ news_app/lib/constants/app_colors.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color orange = Color(0xfffe5722);
+  static const Color black = Color(0xff000000);
+  static const Color whire = Color(0xffffffff);
 }
