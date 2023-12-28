29b75844a92f480a37035b59fe4664481fcc8d55
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-12-28 17:13:32 +0600

    Day 41

--- news_app/lib/app/app.dart
+++ news_app/lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/home/home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -28,6 +29,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return HomePage();
   }
 }
