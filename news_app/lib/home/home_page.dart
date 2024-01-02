42a19cbc8232a56e0ef4bf8c6319cb14561e2f62
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2024-01-02 12:11:00 +0600

    Day 41

--- news_app/lib/home/home_page.dart
+++ news_app/lib/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:news_app/constants/app_colors.dart';
 import 'package:news_app/home/article_detail_page.dart';
 import 'package:news_app/model/fake_data.dart';
+import 'package:news_app/model/news_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -25,24 +26,74 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
+
       body: ListView.builder(
         padding: const EdgeInsets.all(20),
-        itemCount: fakeData.articles.length,
+        itemCount: newsFakeList.length,
         itemBuilder: (context, index) {
-          return InkWell(
-            onTap: () {
-              Navigator.push<void>(
-                context,
-                MaterialPageRoute<void>(
-                  builder: (BuildContext context) => ArticleDetailPage(
-                    fakeData.articles[index],
-                  ),
+          final newModel = newsFakeList[index];
+          return Padding(
+            // ignore: prefer_const_constructors
+            padding: EdgeInsets.only(bottom: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Image.network(
+                        newModel.image,
+                        fit: BoxFit.fitHeight,
+                      ),
+                    ),
+                    const SizedBox(width: 15),
+                    Expanded(
+                      flex: 3,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            newModel.title,
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                          Text(
+                            newModel.description,
+                          ),
+                          Text(
+                            newModel.dateTime,
+                            style: Theme.of(context).textTheme.titleSmall,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              );
-            },
+                const SizedBox(height: 7),
+                const Divider(color: AppColors.black),
+              ],
+            ),
           );
         },
       ),
+      // body: ListView.builder(
+      //   padding: const EdgeInsets.all(20),
+      //   itemCount: fakeData.articles.length,
+      //   itemBuilder: (context, index) {
+      //     return InkWell(
+      //       onTap: () {
+      //         Navigator.push<void>(
+      //           context,
+      //           MaterialPageRoute<void>(
+      //             builder: (BuildContext context) => ArticleDetailPage(
+      //               fakeData.articles[index],
+      //             ),
+      //           ),
+      //         );
+      //       },
+      //     );
+      //   },
+      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: AppColors.orange,
