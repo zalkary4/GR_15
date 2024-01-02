26d616d4a61b25f0a61a7c14de90534ffbf9a762
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2024-01-02 12:16:33 +0600

    Day 41

--- news_app/lib/home/home_page.dart
+++ news_app/lib/home/home_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/constants/app_colors.dart';
-import 'package:news_app/home/article_detail_page.dart';
-import 'package:news_app/model/fake_data.dart';
 import 'package:news_app/model/news_model.dart';
+import 'package:news_app/widgets/news_card_2.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -57,58 +56,6 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: AppColors.orange,
         foregroundColor: AppColors.white,
         child: const Icon(Icons.search),
-      ),
-    );
-  }
-}
-
-class NewsCard2 extends StatelessWidget {
-  const NewsCard2(this.newModel, {super.key});
-
-  final NewModel newModel;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      // ignore: prefer_const_constructors
-      padding: EdgeInsets.only(bottom: 20),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Expanded(
-                flex: 2,
-                child: Image.network(
-                  newModel.image,
-                  fit: BoxFit.fitHeight,
-                ),
-              ),
-              const SizedBox(width: 15),
-              Expanded(
-                flex: 3,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      newModel.title,
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                    Text(
-                      newModel.description,
-                    ),
-                    Text(
-                      newModel.dateTime,
-                      style: Theme.of(context).textTheme.titleSmall,
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 7),
-          const Divider(color: AppColors.black),
-        ],
       ),
     );
   }
