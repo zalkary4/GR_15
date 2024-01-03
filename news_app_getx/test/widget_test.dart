ad549fc31b492c022151deb50854039a2e1279ab
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2024-01-03 10:16:02 +0600

    Day 41

--- news_app_getx/test/widget_test.dart
+++ news_app_getx/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:news_app_getx/main.dart';
+import 'package:news_app_getx/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
