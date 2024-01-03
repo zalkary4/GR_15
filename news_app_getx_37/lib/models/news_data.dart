b50262d2a93bbbbc820abfacee7fc008f4589b4c
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2024-01-03 13:48:01 +0600

    update

---
+++ news_app_getx_37/lib/models/news_data.dart
@@ -0,0 +1,42 @@
+class NewsData {
+  NewsData({
+    required this.status,
+    required this.totalResults,
+    required this.articles,
+  });
+  String status;
+  int totalResults;
+  List<Article> articles;
+}
+
+class Article {
+  Source source;
+  String? author;
+  String title;
+  String? description;
+  String url;
+  String? urlToImage;
+  DateTime publishedAt;
+  String? content;
+
+  Article({
+    required this.source,
+    required this.author,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.urlToImage,
+    required this.publishedAt,
+    required this.content,
+  });
+}
+
+class Source {
+  String? id;
+  String name;
+
+  Source({
+    required this.id,
+    required this.name,
+  });
+}
