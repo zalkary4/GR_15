4fa385b91004511b5f1868e2595991429b2768b2
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2024-01-16 17:12:28 +0600

    les 40

--- bloc_3/lib/second/second_page.dart
+++ bloc_3/lib/second/second_page.dart
@@ -9,7 +9,7 @@ class SecondPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Second Page'),
+        title: const Text('Second Page'),
       ),
       body: Center(
         child: Column(
