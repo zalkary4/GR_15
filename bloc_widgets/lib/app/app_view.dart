0abe9caed8b36fe15bd2bfd5da307b403f42b17b
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2024-01-20 10:43:30 +0600

    les 40

--- bloc_widgets/lib/app/app_view.dart
+++ bloc_widgets/lib/app/app_view.dart
@@ -1,17 +1,22 @@
 import 'package:bloc_widgets/home/home_view.dart';
+import 'package:bloc_widgets/logic/home_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (context) => HomeCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomeView(),
       ),
-      home: const HomeView(),
     );
   }
 }
