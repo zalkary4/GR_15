5eacd945268920eaa8465087adfb5b7a235dd7eb
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2024-01-21 15:53:12 +0600

    les 40

--- bloc_widgets/lib/home/home_view.dart
+++ bloc_widgets/lib/home/home_view.dart
@@ -9,34 +9,46 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home view')),
-      body: Column(
-        children: [
-          BlocBuilder<HomeCubit, HomeState>(
-            builder: (context, state) {
-              if (state is HomeInitialState) {
-                return const Text('Inintial');
-              } else if (state is HomeLoadingState) {
-                return const CircularProgressIndicator();
-              } else if (state is HomeSuccessState) {
-                return Text('success ${state.data}');
-              } else if (state is HomeErrorState) {
-                return Text('error, ${state.message}');
-              } else {
-                return const Text('unknown');
-              }
-              // return Text(state.toString());
-            },
-          ),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                final homeCubit = context.read<HomeCubit>();
-                homeCubit.parseNumber('234e');
+      body: BlocListener<HomeCubit, HomeState>(
+        listener: (context, state) {
+          if (state is HomeErrorState) {
+            showAdaptiveDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: Text('Something wrong'),
+                content: Text('Value is ${state.message}'),
+              ),
+            );
+          }
+        },
+        child: Column(
+          children: [
+            BlocBuilder<HomeCubit, HomeState>(
+              builder: (context, state) {
+                if (state is HomeInitialState) {
+                  return const Text('Inintial');
+                } else if (state is HomeLoadingState) {
+                  return const CircularProgressIndicator();
+                } else if (state is HomeSuccessState) {
+                  return Text('success ${state.data}');
+                } else if (state is HomeErrorState) {
+                  return Text('error, ${state.message}');
+                } else {
+                  return const Text('unknown');
+                }
               },
-              child: const Text('parse'),
             ),
-          ),
-        ],
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  final homeCubit = context.read<HomeCubit>();
+                  homeCubit.parseNumber('234s');
+                },
+                child: const Text('parse'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
