1fa97e429c73f905169ff7f98ebfce2ed877963b
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2024-01-25 16:39:11 +0600

    les 42

--- flutter_infinite_list/lib/posts/bloc/post_bloc.dart
+++ flutter_infinite_list/lib/posts/bloc/post_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_infinite_list/posts/models/post.dart';
@@ -15,10 +17,28 @@ class PostBloc extends Bloc<PostEvent, PostState> {
   }
   final Client httpClient;
 
-  Future<List<Post>> _fetchStatus([
-    int startIndex = 0,
-  ]) async {
-    final uri = Uri.parse(
-        'https://jsonplaceholder.typicode.com/posts?_start=$startIndex&_limit=$_postLimit');
+  Future<List<Post>> _fetchStatus([int startIndex = 0]) async {
+    final response = await httpClient.get(
+      Uri.https(
+        'jsonplaceholder.typicode.com',
+        '/posts',
+        <String, String>{'_start': '$startIndex', '_limit': '$_postLimit'},
+      ),
+    );
+    if (response.statusCode == 200) {
+      final body = json.decode(response.body) as List;
+      return body.map((dynamic e) {
+        final json = e as Map<String, dynamic>;
+        return Post(
+          userId: json['userId'],
+          id: json['id'],
+          title: json['title'],
+          body: json['body'],
+        );
+      }).toList();
+    }
+    throw Exception('error fetching posts');
   }
+  // 'https://jsonplaceholder.typicode.com/posts?_start=$startIndex&_limit=$_postLimit');
+  // final response = await httpClient;
 }
