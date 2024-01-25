2c4badb76d3f9682a9ac5f04f267b19feb5657c9
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2024-01-25 15:36:16 +0600

    les 42

--- flutter_infinite_list/lib/posts/bloc/post_bloc.dart
+++ flutter_infinite_list/lib/posts/bloc/post_bloc.dart
@@ -1,5 +1,12 @@
+import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_infinite_list/posts/models/post.dart';
 
 part 'post_event.dart';
 part 'post_state.dart';
+
+class PostBloc extends Bloc<PostEvent, PostState> {
+  PostBloc() : super(PostState()) {
+    on<PostEvent>((event, emit) {});
+  }
+}
